2619925321fb833d5c9d0e697626a67a21621bf7
Dobando-sys <obandodesart@gmail.com>
2025-03-25 19:37:51 -0500

    Update fibonacci.java
    Se modificó para que se impriman N cantidad de números de la serie Fibonacci indicada por el usuario.

--- fibonacci.java
+++ fibonacci.java
@@ -7,20 +7,20 @@ public class fibonacci {
         System.out.println("Indique un número límite para calcular la serie de Fibonacci: ");
         int numeri = sc.nextInt();
 
-        // Variable que representa el número actual de la serie
-        int sumador = 1;
-        // Variable que representa el número anterior de la serie
-        int anterior = 1;
+        int sumador = 1; // Variable que representa el número actual de la serie
+        int anterior = 0; // Variable que representa el número anterior de la serie
+        int contador =1; // Variable que lleva un seguimiento del número de iteraciones
         sc.close();
 
-        System.out.println("Su serie de Fibonnacci ");
+        System.out.println("Los primeros " + numeri + " números de la serie de Fibonacci son: ");
         System.out.println("0");
         do {
             System.out.println(sumador);
             sumador = anterior + sumador; // Suma el número actual con el anterior
-            anterior = sumador - anterior; // Se calcula la diferencia entre el nuevo número actual y el anterior
-            
-        } while (sumador <= numeri);
+            anterior = sumador - anterior; // Se devuelve la suma para llevar registro del número que va antes en la serie.
+            contador++; //Lleva un seguimiento del número de iteraciones
+
+        } while (contador < numeri); //Se repite hasta que se alcance el número de iteraciones indicado por el usuario
         
     }
 }
